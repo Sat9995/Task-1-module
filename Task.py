2b47e742211cbde7bc484743fd233e588dc5b8ac
Sat9995 <saidboy995@gmail.com>
2024-10-30 08:35:30 -0700

    Add files via upload

---
+++ Task.py
@@ -0,0 +1,11 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+my_list=list(students)
+sorted_list=sorted(my_list)
+print(sorted_list)
+grades = [4,2,4,3,4]
+grades_students={'Aaron':((5+3+3+5+4)/5), 'Bilbo':((2+2+2+3)/4), 'Johnny':((2+2+2+3)/4), 'Khendrik':((4+4+3)/3), 'Steve':((5+5+5+4+5)/5)}
+print(grades_students)
+
+
+
